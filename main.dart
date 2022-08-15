c0942a2496312f92c8b51f8c8f60ccc49c3c4574
How To Flutter <109933224+How-To-Flutter@users.noreply.github.com>
2022-08-15 15:46:45 -0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,22 @@
+import 'package:crypto_tracker/homeScreen.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        systemNavigationBarColor: Colors.black));
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    );
+  }
+}
